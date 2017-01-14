2e3c44943f77fc4c1daaca382e483533cd029205
edsonmarcks <edsonmarcks@debian.com>
2017-01-14 09:26:46 -0300

    criação do controle de material

--- src/controle/ControleMaterial.java
+++ src/controle/ControleMaterial.java
@@ -5,10 +5,46 @@
  */
 package controle;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.function.Predicate;
+import modelo.Material;
+import modelo.data.interfaces.IControle;
+
 /**
  *
  * @author edsonmarcks
  */
-public class ControleMaterial {
+public class ControleMaterial implements IControle<Material>{
+
+    @Override
+    public void saveUpdate(Material entity) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Material entity) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Material findById(long id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Material> getAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Material> getAll(Predicate<Material> predicate) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Material find(Predicate<Material> predicate) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
